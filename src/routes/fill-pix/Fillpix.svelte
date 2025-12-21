6a0e6a8c444cf2aca886ce7d5c8abd3edb75f872
Scott Wu <sw@scottwu.ca>
2025-12-21 13:47:23 +0800

    .

--- src/routes/fill-pix/Fillpix.svelte
+++ src/routes/fill-pix/Fillpix.svelte
@@ -5,8 +5,8 @@
 	const game = new FillPixGame()
 </script>
 
-<div class="flex h-screen flex-col items-center p-4 dark:bg-gray-900 dark:text-white">
-	<h1 class="mb-4 text-2xl font-bold">Fill-Pix Puzzle</h1>
+<div class="flex h-screen flex-col items-center p-4 dark:bg-gray-900">
+	<h1 class="mb-4 text-2xl font-bold dark:text-white">Fill-Pix Puzzle</h1>
 	<div class="grid" style="grid-template-columns: repeat({game.grid.width}, minmax(0, 1fr))">
 		{#each game.grid.cells as row, _r (_r)}
 			{#each row as cell, _c (cell.row + '-' + cell.col)}
