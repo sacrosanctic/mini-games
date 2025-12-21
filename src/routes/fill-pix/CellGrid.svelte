53ee3920ffff3d39e1b24abb0373a8e0b357f1cc
Scott Wu <sw@scottwu.ca>
2025-12-21 12:44:10 +0800

    .

--- src/routes/fill-pix/CellGrid.svelte
+++ src/routes/fill-pix/CellGrid.svelte
@@ -3,32 +3,17 @@
 	import { Cell as CellClass } from './fillpix.svelte.ts'
 </script>
 
-<div class="grid grid-cols-3 gap-2">
+<div class="grid w-fit grid-cols-3 gap-2">
 	{#each [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as value}
-		{@const unmarked = new CellClass({
-			row: 0,
-			col: 0,
-			value,
-			state: 'unmarked',
-			getMarkedCount: (cell) => 0,
-		})}
-		{@const marked = new CellClass({
-			row: 0,
-			col: 0,
-			value,
-			state: 'marked',
-			getMarkedCount: (cell) => 0,
-		})}
-		{@const blocked = new CellClass({
-			row: 0,
-			col: 0,
-			value,
-			state: 'blocked',
-			getMarkedCount: (cell) => 0,
-		})}
-
-		<Cell cell={unmarked} />
-		<Cell cell={marked} />
-		<Cell cell={blocked} />
+		{#each ['unmarked', 'marked', 'blocked'] as const as state}
+			{@const cell = new CellClass({
+				row: 0,
+				col: 0,
+				value,
+				state,
+				getMarkedCount: () => 0,
+			})}
+			<Cell {cell} />
+		{/each}
 	{/each}
 </div>
