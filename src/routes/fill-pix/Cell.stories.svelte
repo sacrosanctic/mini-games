d3720112ff4eb8465d5969e05dded2b722eab7e1
Scott Wu <sw@scottwu.ca>
2025-12-21 12:38:59 +0800

    .

--- src/routes/fill-pix/Cell.stories.svelte
+++ src/routes/fill-pix/Cell.stories.svelte
@@ -1,23 +1,11 @@
 <script module>
 	import { defineMeta } from '@storybook/addon-svelte-csf'
-	import Component from './Cell.svelte'
-	import { Cell } from './fillpix.svelte.ts'
+	import component from './CellGrid.svelte'
 
 	const { Story } = defineMeta({
 		title: 'FillPix/Cell',
-		component: Component,
-		args: {},
+		component,
 	})
 </script>
 
-<Story
-	name="Unmarked-light"
-	args={{ cell: new Cell({ row: 0, col: 0, value: 1, getMarkedCount: () => 9 }) }}
-	globals={{ theme: 'light' }}
-/>
-
-<Story
-	name="Unmarked-dark"
-	args={{ cell: new Cell({ row: 0, col: 0, value: 1, getMarkedCount: () => 9 }) }}
-	globals={{ theme: 'dark' }}
-/>
+<Story name="Cell" />
---
+++ src/routes/fill-pix/CellGrid.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Cell from './Cell.svelte'
+	import { Cell as CellClass } from './fillpix.svelte.ts'
+</script>
+
+<div class="grid grid-cols-3 gap-2">
+	{#each [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as value}
+		{@const unmarked = new CellClass({
+			row: 0,
+			col: 0,
+			value,
+			state: 'unmarked',
+			getMarkedCount: (cell) => 0,
+		})}
+		{@const marked = new CellClass({
+			row: 0,
+			col: 0,
+			value,
+			state: 'marked',
+			getMarkedCount: (cell) => 0,
+		})}
+		{@const blocked = new CellClass({
+			row: 0,
+			col: 0,
+			value,
+			state: 'blocked',
+			getMarkedCount: (cell) => 0,
+		})}
+
+		<Cell cell={unmarked} />
+		<Cell cell={marked} />
+		<Cell cell={blocked} />
+	{/each}
+</div>
