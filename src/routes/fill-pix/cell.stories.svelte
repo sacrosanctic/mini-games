f90d36cc2aea66ddf84eaaaa21fc247c8903320c
Scott Wu <sw@scottwu.ca>
2025-12-24 13:25:44 +0800

    .

--- src/routes/fill-pix/cell.stories.svelte
+++ src/routes/fill-pix/cell.stories.svelte
@@ -6,13 +6,21 @@
 	const { Story } = defineMeta({
 		title: 'FillPix/Cell',
 		component: Cell,
-		args: { onclick: fn() },
+		args: {
+			onclick: fn(),
+			state: {
+				control: { type: 'select' },
+				options: ['unmarked', 'marked', 'blocked'],
+			},
+		},
 	})
 </script>
 
 <Story name="Cell">
 	{#snippet template(args)}
 		<div class="grid w-fit grid-cols-6 gap-2">
+			<!-- https://github.com/theetrain/svelte-cartesian/tree/main?tab=readme-ov-file#before-using-svelte-cartesian -->
+			<!-- reduce loops -->
 			{#each [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as hint (hint)}
 				{#each [false, true] as isComplete (isComplete)}
 					{#each ['unmarked', 'marked', 'blocked'] as const as state (state)}
@@ -23,3 +31,5 @@
 		</div>
 	{/snippet}
 </Story>
+
+<Story name="Cell2" />
--- src/routes/fill-pix/cell.svelte
+++ src/routes/fill-pix/cell.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { Attachment } from 'svelte/attachments'
 	import { twMerge } from 'tailwind-merge'
 
 	interface Props {
@@ -9,13 +10,30 @@
 	}
 
 	let { hint, state, isComplete = false, onclick }: Props = $props()
+
+	const flashRed: Attachment<HTMLElement> = (node) => {
+		let first = true
+		$effect(() => {
+			void state
+			if (first) first = false
+			else {
+				node.style.transition = 'none'
+				node.classList.add('!bg-red-500')
+				setTimeout(() => {
+					node.style.transition = 'background-color 1s ease-out'
+					node.classList.remove('!bg-red-500')
+				}, 500)
+			}
+		})
+	}
 </script>
 
 <button
+	{@attach flashRed}
 	class={twMerge(
-		'relative flex size-10 cursor-pointer items-center justify-center border  text-3xl',
+		'relative flex size-10 cursor-pointer items-center justify-center border text-3xl',
 		'border-gray-300 bg-white dark:border-gray-500 dark:bg-gray-800 dark:text-white',
-		state === 'marked' && 'bg-black text-white dark:bg-gray-600 ',
+		state === 'marked' && 'bg-black text-white dark:bg-gray-600',
 		isComplete && 'text-gray-300 dark:text-gray-500',
 	)}
 	{onclick}
